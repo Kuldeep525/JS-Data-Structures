e284c8c4c7bbb7cc6e3400fe9a969d9eedd3545f
Kuldeep Pandey <84696370+Kuldeep525@users.noreply.github.com>
2021-08-10 02:32:46 +0530

    Update RepeatedNumber.js

--- Array/RepeatedNumber.js
+++ Array/RepeatedNumber.js
@@ -1,3 +1,5 @@
+// Problem 5. Take a range from 0 – 100, find the digits that are repeated twice like 33, 77,etc and store them in an array
+
 let arrayOfNumber = [];
 for(i = 0 ; i <= 100 ; i++){
    let a = i % 11 ;
